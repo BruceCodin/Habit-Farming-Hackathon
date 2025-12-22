41cf11c7ae197975530b0e7636d0935c6fa58daf
BruceCodin <127963534+BruceCodin@users.noreply.github.com>
2025-12-22 17:05:01 +0000

    feat: rds terraform code

---
+++ terraform/rds.tf
@@ -0,0 +1,88 @@
+# rds terraform set up
+
+# Get the default VPC (Virtual Private Cloud) - Cohort 20 VPC
+data "aws_vpc" "cohort_vpc" {
+  id = "vpc-01b7a51a09d27de04"
+}
+
+# Get all subnets in the VPC - Cohort 20 Subnets
+data "aws_subnets" "cohort_subnets" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.cohort_vpc.id]
+  }
+}
+
+# Subnet group
+resource "aws_db_subnet_group" "tamagotchi_subnet_group" {
+  name       = var.subnet_group_name
+  subnet_ids = data.aws_subnets.cohort_subnets.ids
+
+  tags = {
+    Name = var.subnet_group_name
+  }
+}
+
+# Security group (PUBLIC)
+resource "aws_security_group" "tamagotchi_db_sg" {
+  name        = var.security_group_name
+  description = "Allow PUBLIC PostgreSQL access (hackathon only)"
+  vpc_id      = data.aws_vpc.cohort_vpc.id
+
+  ingress {
+    from_port   = var.db_port
+    to_port     = var.db_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = var.security_group_name
+  }
+}
+
+
+# RDS instance
+resource "aws_db_instance" "hackathon_db" {
+  identifier        = var.db_identifier
+  engine            = "postgres"
+  engine_version    = var.engine_version
+  instance_class    = var.db_instance_class
+  allocated_storage = var.allocated_storage
+  storage_type      = "gp2"
+
+  db_name  = var.db_name
+  username = var.db_username
+  password = var.db_password
+
+  vpc_security_group_ids = [aws_security_group.tamagotchi_db_sg.id]
+  db_subnet_group_name = aws_db_subnet_group.tamagotchi_subnet_group.name
+
+  publicly_accessible  = true
+  skip_final_snapshot  = true
+  deletion_protection  = false
+  apply_immediately    = true
+  backup_retention_period = 0
+
+  tags = {
+    Name = var.db_identifier
+  }
+}
+
+# Outputs
+output "db_endpoint" {
+  value       = aws_db_instance.hackathon_db.endpoint
+  description = "The connection endpoint for the RDS instance"
+}
+
+output "db_name" {
+  value       = aws_db_instance.hackathon_db.db_name
+  description = "The database name"
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,60 @@
+# variables.tf - Variables for our terraform file 
+
+# main.tf confirguration
+variable "aws_region" {
+  description = "AWS region for deploying resources"
+  type        = string
+  default     = "eu-west-2"
+}
+
+# rds.tf confirguration 
+variable "db_identifier" {
+  type    = string
+  default = "tamagotchi-database"
+}
+
+variable "db_instance_class" {
+  type    = string
+  default = "db.t3.micro"
+}
+
+variable "db_name" {
+  type    = string
+  default = "tamagotchi"
+}
+
+variable "db_username" {
+  type    = string
+  default = "postgres"
+}
+
+variable "db_password" {
+  type        = string
+  sensitive   = true
+  description = "Master password for the DB"
+}
+
+variable "allocated_storage" {
+  type    = number
+  default = 20
+}
+
+variable "engine_version" {
+  type    = string
+  default = "16.3"
+}
+
+variable "db_port" {
+  type    = number
+  default = 5432
+}
+
+variable "subnet_group_name" {
+  type    = string
+  default = "hackathon-db-subnet-group"
+}
+
+variable "security_group_name" {
+  type    = string
+  default = "hackathon-db-public-sg"
+}
