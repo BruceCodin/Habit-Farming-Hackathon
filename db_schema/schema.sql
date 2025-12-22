cb7b7cc6ac0eb7c84526250eda67a5ab8b8e1b38
BruceCodin <127963534+BruceCodin@users.noreply.github.com>
2025-12-22 15:40:50 +0000

    feat: schema sql added

---
+++ db_schema/schema.sql
@@ -0,0 +1,45 @@
+-- Schema code for setting up the database for Hackathon Project: Tamagotchi Habit Tracker
+
+DROP TABLE IF EXISTS habits CASCADE;
+DROP TABLE IF EXISTS tamagotchi CASCADE;
+DROP TABLE IF EXISTS habit_completion CASCADE;
+
+CREATE TABLE habits (
+    habit_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    habit_name TEXT NOT NULL,
+    habit_description TEXT,
+    target_frequency INT NOT NULL,
+    frequency_unit TEXT NOT NULL,
+    is_active BOOLEAN NOT NULL DEFAULT TRUE,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),  
+
+    -- Constraints: 
+    CONSTRAINT target_frequency_check CHECK (target_frequency >= 0)
+
+);
+
+CREATE TABLE tamagotchi (
+    tamagotchi_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    habit_id INT NOT NULL,
+    tamagotchi_name TEXT NOT NULL,
+    happiness_level INT NOT NULL DEFAULT 0,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+
+    -- Habit ID Foreign Key: 
+    FOREIGN KEY (habit_id) REFERENCES habit(habit_id) ON DELETE CASCADE,
+    -- Constraints:
+    CONSTRAINT happiness_level_check CHECK (happiness_level >= 0)
+
+);
+
+
+CREATE TABLE habit_completion (
+    completion_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    habit_id INT NOT NULL,
+    completion_date DATE NOT NULL,
+    completed_at TIMESTAMP NOT NULL,
+
+    -- Habit ID Foreign Key: 
+    FOREIGN KEY (habit_id) REFERENCES habit(habit_id) ON DELETE CASCADE
+
+);
