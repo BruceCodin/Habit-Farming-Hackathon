dcd1f3e6f57ad3451e0080b5c4a6919af144cae7
Sufyan <trainee.sufyan.shah@sigmalabs.co.uk>
2025-12-22 15:17:22 +0000

    get_db_connection Function

--- backend/database.py
+++ backend/database.py
@@ -1,10 +1,19 @@
+from os import environ as ENV
 from psycopg2 import connect
 from psycopg2 import sql
 from psycopg2.extras import RealDictCursor
 from psycopg2.extensions import connection
+import os 
 
 
 def get_db_connection() -> connection:
+    # return connect(
+    #     dbname=os.environ["DB_NAME"],
+    #     user=os.environ["DB_USER"],
+    #     password=os.environ["DB_PASSWORD"],
+    #     host=os.environ.get("DB_HOST"),
+    #     port=os.environ.get("DB_PORT", 5432)
+    # )
     ...
 
 
